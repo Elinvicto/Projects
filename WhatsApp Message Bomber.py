ef6be4ced21db6d177bf62e6777c352d55268060
Elinvicto <93968271+Elinvicto@users.noreply.github.com>
2023-03-20 17:29:20 +0530

    Must Edit message before running

--- WhatsApp Message Bomber.py
+++ WhatsApp Message Bomber.py
@@ -1,9 +1,10 @@
+#Edit message before running program
 import pyautogui as pg 
 import webbrowser as wb
 import time
-wb.open("https://web.whatsapp.com/")
-time.sleep(0.01)
-for i in range(200):
-    pg.write("nahi jat")
-    time.sleep(0.01)
+wb.open("https://web.whatsapp.com/") #Delivery website
+time.sleep(0.01) #Timer
+for i in range(200): #Number of messages
+    pg.write("nahi jat") #Message Content
+    time.sleep(0.01) 
     pg.press("Enter")
